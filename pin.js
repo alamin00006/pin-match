55a7c58486f8db287bbf6a891966a21438195854
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-02-14 00:55:55 +0600

    pin match

---
+++ pin.js
@@ -0,0 +1,45 @@
+
+function myGenerator(){
+  
+     const randomNumber = Math.round(Math.random()*10000);
+            let inputFiled = document.getElementById('input-filed');
+            inputFiled.value = randomNumber;
+           
+    // if(inputFiled.value==4){
+       
+    // }
+    // else{
+    //     myGenerator();
+    // }        
+   
+}
+document.getElementById('calc-number').addEventListener('click', function(event){
+ const number = event.target.innerText;
+ let inputCalc = document.getElementById('input-clac');
+ if(isNaN(number)){
+    if(number=='C'){
+        inputCalc.value = '';
+    }
+ }
+ else{
+    inputCalc.value =  inputCalc.value + number;
+ }
+ 
+})
+// submit
+document.getElementById('submit-Button').addEventListener('click', function(){
+    let inputCalc = document.getElementById('input-clac').value; 
+    let inputFiled = document.getElementById('input-filed').value;
+    if(inputCalc == inputFiled){
+     document.getElementById("match").style.display = 'block';
+     document.getElementById("notify-notmatch").style.display = 'none';
+        
+    }
+    else{
+        document.getElementById("match").style.display = 'none';
+    document.getElementById("notify-notmatch").style.display = 'block';
+      
+   
+    }
+})
+
