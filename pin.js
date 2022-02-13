271279f183076fba4f69d39ecec62b7d3dc609a6
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-02-14 01:15:42 +0600

    final done

--- pin.js
+++ pin.js
@@ -2,15 +2,17 @@
 function myGenerator(){
   
      const randomNumber = Math.round(Math.random()*10000);
+       const pin = randomNumber + '';
             let inputFiled = document.getElementById('input-filed');
-            inputFiled.value = randomNumber;
+            inputFiled.value = pin;
            
-    // if(inputFiled.value==4){
+    if(inputFiled.value.length==4){
+       return inputFiled.value;
+    }
+    else{
        
-    // }
-    // else{
-    //     myGenerator();
-    // }        
+      return myGenerator();
+    }        
    
 }
 document.getElementById('calc-number').addEventListener('click', function(event){
